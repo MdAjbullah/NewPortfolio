a1dc876ab0cff05a5b59ba83547afdf17aa0c035
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2025-02-19 15:59:00 +0530

    Add files via upload
    biuld file

---
+++ src/data/constants.js
@@ -0,0 +1,342 @@
+export const Bio = {
+  name: "Md Ajbullah Mansuri",
+  roles: [
+    "Full Stack Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/MdAjbullah",
+  resume:
+    "https://drive.google.com/file/d/1TcOWU3YdFftqXerG2-yp-z0WJWFL-dXN/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/mdajbullah/",
+  twitter: "https://x.com/MdAjbullah",
+  insta: "https://www.instagram.com/rajjajbullah/?next=%2F",
+  facebook: "https://www.facebook.com/ajbullah.rajj?mibextid=ZbWKwL",
+  youtube:"www.youtube.com/@econcepts4u830"
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Material UI",
+        image:
+          "https://static-00.iconduck.com/assets.00/material-ui-icon-2048x1626-on580ia9.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://w7.pngwing.com/pngs/846/87/png-transparent-mean-solution-stack-express-js-node-js-javascript-github-text-trademark-logo.png"
+      },
+      {
+        name: "Core Java",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQfPubhH7QOXhY-TgYSBOTuvlT_ev-9SOJ3w&usqp=CAU",
+      },
+    ],
+  },
+  {
+    title: "Database",
+    skills: [
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "SQL",
+        image:
+          "https://seeklogo.com/images/A/azure-sql-database-logo-D7A32C9CD9-seeklogo.com.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvb-AAIFFETEbbKf65WtEmY5q1xg1XFghyDQ&usqp=CAU",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://cdn.worldvectorlogo.com/logos/postman.svg",
+      },
+      {
+        name: "Adobe XD",
+        image:
+          "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://cdn3d.iconscout.com/3d/free/thumb/free-figma-5562382-4642727.png?f=webp",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b02c5595-0db5-4537-aa5b-d610fc51bfd7/ddqrh8c-6e1d29f8-9813-45e9-871e-d9bfe1d499f6.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2IwMmM1NTk1LTBkYjUtNDUzNy1hYTViLWQ2MTBmYzUxYmZkN1wvZGRxcmg4Yy02ZTFkMjlmOC05ODEzLTQ1ZTktODcxZS1kOWJmZTFkNDk5ZjYuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.poY4dEAGvbUpVzrKOHtS2xDKCS9xioteqOrV5E68RhQ",
+    role: "MERN STACK WEB DEVELOPMENT INTERN",
+    company: "Jspider, Delhi",
+    date: "JULY 2024- Present ",
+    desc: "Trained by excellent trainerS on web development tools and completed many projects",
+    skills: [
+      "HTML5",
+      "CSS3",
+      "JavaScript",
+      "ES6",
+      "React Js",
+      "Redux",
+      "Node.js",
+      "Express.js",
+      "MongoDb",
+    ],
+    doc:"https://drive.google.com/file/d/1wWQ9nI8Vy0dJIYpcDWt8I89mu2Utf1V8/view?usp=sharing",
+  },
+  {
+    id: 1,
+    img: "https://asset-cdn.learnyst.com/assets/schools/134719/schoolLogo/200_200_e7ma0m.png",
+    role: "JAVA FULL STACK DEVELOPMENT INTERN",
+    company: "Tap Academy Private Limited",
+    date: "FEB 2024 - DEC 2024",
+    desc: "Getting tained by Excellent trainer on Software Development",
+    skills: [
+      "Java Core & Advance",
+      "SQL",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React JS",
+    ],
+    doc: "https://drive.google.com/file/d/106isK-lhnf63Di0pq5sIVEuzL57OnAoK/view?usp=sharing", 
+  }, 
+  {
+    id: 2,
+    img: "https://th.bing.com/th/id/OIP.VnnbWLtFH0YkSHdqA3cA8wAAAA?w=300&h=96&rs=1&pid=ImgDetMain",
+    role: "JAVA  DEVELOPMENT INTERN",
+    company: "INFOWIZE",
+    date: "JUN-2019 - AUG-2019",
+    desc: "Getting tained by Excellent trainer on Software Development",
+    skills: [
+      "Java Core",
+      "SQL",
+      "HTML",
+      "CSS",
+     
+    ],
+    doc: "https://drive.google.com/file/d/106isK-lhnf63Di0pq5sIVEuzL57OnAoK/view?usp=sharing", 
+  }, 
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.jct.ac.in/engineering/wp-content/themes/jct-engineering/images/logo.png",
+    school: "JCT College Of Engineering and Technology, Pichanur, Coimbatore, (Affiliated with Anna University, Chennai)",
+    date: "Sept 2020 - Jul 2024",
+    grade: "8.0 CGPA",
+    desc: "I have completed Bachelor Of Engineering in Computer Science and Engineering department at JCT College of Engineering and Technology, Coimbatore. I have scored total 8.0 CGPA. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. And I have achieved the 2nd best Academic Award from JCT College of Engineering and Technology in CSE Department(2020-2024).",
+    degree: "Bachelor of Engineering - B.E. , Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://iconape.com/wp-content/png_logo_vector/emax-electronics-logo.png",
+    school: "E-max Polytechnic College  Ambala, Haryana ",
+    date: "SEP 2017 - Jul 2020",
+    grade: "68.0%",
+    desc: "I completed my diploma (Computer science & engineering ) from E-max Polytechnic College  Ambala, Haryana .",
+    degree: "Diploma in computer science (Diploma)",
+  },
+  {
+    id: 2,
+    img: "https://static.toiimg.com/thumb/msid-62801052,width-400,resizemode-4/62801052.jpg",
+    school: "L.N.D College Motihari ,  E. Champaran",
+    date: "Apr 2015 - Apr 2017",
+    grade: "65.60%",
+    desc: "I completed my 12(Intermediate of Science(PCMB)) with , LND college Motihari, East Champaran.", 
+    degree: "intermediate (XII)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Weather App",
+    date: "Aug 2023 - Aug 2023",
+    description:
+      "This is developed using Weather Api and I have used technologies like: HTML, CSS and JavaScript only. It will fetch the data according to place name and provide the weather report according to specific place/city. ",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmkglqS2svtuNS9TP3jTSBHjHJYraFNkVnhw&usqp=CAU",
+    tags: [
+      "Weather API",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    category: "web app",
+    github: "",
+  },
+  {
+    id: 0,
+    title: "ToDo Web App",
+    date: "Sept 2023 - Sept 2023",
+    description:
+      "This is developed using React Components and hooks where any user can perform add and delete like (add or remove to cart) operation .",
+    image:
+      "https://www.freecodecamp.org/news/content/images/2020/07/cover-1.png",
+    tags: ["React Js", "HTML", "CSS"],
+    category: "web app",
+    // github: "",
+    // webapp: "",
+  },
+  {
+    id: 1,
+    title: "My Family Web Page",
+    date: "Aug 2023 - Aug 2023",
+    description:
+      "Designed and developed the my Family Wwb Pages, where many sections are included with them like intro of all members, galery sections, their professions ...",
+    image:
+      "https://149842033.v2.pressablecdn.com/wp-content/uploads/2020/01/modern-housewife-family-reunion-website-template.jpg",
+    tags: [
+      "HTML",
+      "CSS",
+      "Bootstrap",
+    ],
+    category: "web app",
+    github: "",
+    // webapp: "",
+  },
+  {
+    id: 2,
+    title: "Clone of Amazon",
+    date: "Oct 2023 - Oct 2023",
+    description:
+      "This Amazon front-end I developed using HTML, CSS, React JS and Material UI, where I tried to completely look like real Amazon page.",
+    image:
+      "https://www.versionmuseum.com/images/websites/amazon-website/amazon-website%5E2019%5Ehomepage.jpg",
+    tags: ["HTML", "CSS", "React", "Bootstrap","Material UI"],
+    category: "web app",
+    // github: "",
+    // webapp: "",
+  },
+  {
+    id: 3,
+    title: "CRUD Operation in React",
+    date: "Nov 2023 - Nov 2023",
+    description:
+      "Here in this web application we can perform CRUD(Create, Read, Update and Delete) Operation which means user can add item, delete ites and they can update thier items(data).",
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhK4Fpa8hdFXegKZJrg6mXsk4DRDuyzzs57Q3RQLfvvJneatpDGH6uH72xduF_eA-R15huZMZd1D_h_nmYEx3dzObZGfXJWKYxQNdJ0dfWnAcjUxnrIhM_IhVAY-BmiOAcqpitU0auPW8A/d/Screenshot+07-31-2020+18.31.57.png",
+    tags: ["HTML", "CSS", "React JS"],
+    category: "web app",
+    // github: "",
+    // webapp:"",
+  },
+  {
+    id: 10,
+    title: "Real Time Coomunication System Powered By AI(IBM Project)",
+    date: "Sept 2022 - Nov 2022",
+    description:
+      "This project is done by thorugh IBM. The Real Time Communication System Powered By AI which mean it converts the sign language into English Language, this project will be helopfull for specially abled people who can't speak.",
+    image:
+      "https://i.ytimg.com/vi/-r-pXrfxYk4/maxresdefault.jpg",
+    tags: ["Python", "Machine Learning"],
+    category: "machine learning",
+    github: "",
+    // webapp: "",
+  },
+  {
+    id:4,
+    title:"Sending Mail through Node.js",
+    date:"Jan 2024 - Jan 2024",
+    description:
+      "Using Node js, here we can send the mail to one or more people using Nodemailer module",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKymi6IDPpErWbm4cisgtXOmtrPWcQkqCzqQ&usqp=CAU",
+    tags: ["Node js","Nodemailer module","http module"],
+    category:"backend",
+  },
+  {
+    id:5,
+    title:"User Authentication",
+    date:"Feb 2024 - Feb 2024",
+    description:
+      "Using Express JS we will I have done user authenticate where the data will be stored first inside mongodb database and then fetch from the database and authenticate the users.",
+    image:"https://miro.medium.com/v2/resize:fit:1400/1*sdTMmAqKVy0CV-1MoTV6oA.jpeg",
+    tags: ["Express Js","Mongo DB","HTML","CSS"],
+    category:"backend",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
